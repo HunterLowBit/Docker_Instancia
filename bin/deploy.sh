6201ad20c4e91eb6e25f73a82f1cb84e5c259df3
HunterLowBit_Ubuntu <robsonpereira98@gmail.com>
2023-10-12 16:46:54 -0300

    Removido projeto anterior

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,4 @@
 cd ..
 fileEnv="docker-compose.yaml"
 echo "Deploying docker-compose.yaml"
-docker-compose -f docker-compose.yaml -f $fileEnv up --build  -d
-docker 
+docker-compose -f docker-compose.yaml -f $fileEnv up --build
